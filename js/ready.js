b4dc6ec06aad4c07826597e4723877caab587070
Carlos Cabo <carloscabo@gmail.com>
2013-06-21 20:36:06 +0200

    New samples

--- js/ready.js
+++ js/ready.js
@@ -40,26 +40,84 @@ $(document).ready(function() {
   var locations = [
     {
       idx: 1, // MUST BE UNIQUE AND NUMERIC
-      lat: 40.372,
-      lng: -3.915,
-      iw: 'Universidad Europea de Madrid.<br>Campus de Villaviciosa de Odón<br>C/ Tajo s/n. Villaciosa de Odón<br>28670 Madrid.<br><a href="http://www.uem.es/">Visita la web</a>'
+      lat: 42.5868,
+      lng: 0.9745,
+      iw: 'Aigüestortes i Estany de Sant Maurici.<br>Lérida, Cataluña<br><a href="http://es.wikipedia.org/wiki/Parque_nacional_de_Aig%C3%BCestortes_y_Lago_de_San_Mauricio">Wikipedia</a>'
     },{
       idx: 289,
-      pin: 'orange',
-      lat: 39.479,
-      lng: -0.366,
-      iw: 'Universidad Europea de Valencia<br>C/ General Elio, 2-8-10 46010 Valencia (Frente a los Jardines de Viveros)<br><a href="http://valencia.uem.es" >Visita la web</a>'
+      lat: 39.158333,
+      lng: 2.966667,
+      iw: 'Archipiélago de Cabrera<br>Islas Baleares<a href="http://es.wikipedia.org/wiki/Parque_nacional_mar%C3%ADtimo-terrestre_del_Archipi%C3%A9lago_de_Cabrera">Wikipedia</a>'
     },{
       idx: 38,
+      lat: 39.4261,
+      lng: -4.52528,
+      iw: 'Parque nacional de Cabañeros<br>Toledo, Ciudad Real<br><a href="http://www.parquenacionalcabaneros.com/">Página web oficial</a>'
+    },{
+      idx: 2,
       pin: 'blue',
-      lat: 28.39,
-      lng: -16.524,
-      iw: 'Universidad Europea de Canarias<br>Calle de Inocencio garcía 1. La Orotava. Tenerife.<br><a href="http://universidadeuropeadecanarias.es">visita la web</a>'
+      lat: 28.7166,
+      lng: -17.8833,
+      iw: 'Caldera de Taburiente<br>Islas Canarias<br><a href="http://es.wikipedia.org/wiki/Parque_nacional_de_la_Caldera_de_Taburiente">Wikipedia</a>'
+    },{
+      idx: 3,
+      lat: 37.0003,
+      lng: -6.4166,
+      iw: 'Doñana<br>Huelva<br><a href="http://es.wikipedia.org/wiki/Parque_nacional_y_natural_de_Do%C3%B1ana">Wikipedia</a>'
+    },{
+      idx: 4,
+      pin: 'blue',
+      lat: 28.1262,
+      lng: -17.2372,
+      iw: 'Garajonay<br>Islas Canarias<br><a href="http://es.wikipedia.org/wiki/Parque_nacional_de_Garajonay">Wikipedia</a>'
+    },{
+      idx: 5,
+      pin: 'orange',
+      lat: 42.3805,
+      lng: -8.9333,
+      iw: 'Parque nacional de las Islas Atlánticas de Galicia<br>Galicia<br><a href="http://es.wikipedia.org/wiki/Parque_nacional_de_las_Islas_Atl%C3%A1nticas_de_Galicia">Wikipedia</a>'
+    },{
+      idx: 6,
+      lat: 39.8408,
+      lng: -6.03,
+      iw: 'Parque nacional de Monfragüe<br>Extremadura<br><a href="http://es.wikipedia.org/wiki/Parque_nacional_de_Monfrag%C3%BCe">Wikipedia</a>'
+    },{
+      idx: 7,
+      lat: 42.6716,
+      lng: 0.0555,
+      iw: 'Ordesa y Monte Perdido<br>Huesca<br><a href="http://es.wikipedia.org/wiki/Parque_nacional_de_Ordesa_y_Monte_Perdido">Wikipedia</a>'
+    },{
+      idx: 8,
+      pin: 'orange',
+      lat: 43.1833,
+      lng: -4.8333,
+      iw: 'Picos de Europa<br>Asturias<br><a href="http://es.wikipedia.org/wiki/Picos_de_Europa">Wikipedia</a>'
+    },{
+      idx: 9,
+      lat: 37.2,
+      lng: -3.25,
+      iw: 'Sierra Nevada<br>Granada<br><a href="http://es.wikipedia.org/wiki/Parque_nacional_de_Sierra_Nevada_(Espa%C3%B1a)">Wikipedia</a>'
+    },{
+      idx: 10,
+      lat: 39.1297,
+      lng: -3.7202,
+      iw: 'Tablas de Daimiel<br>Ciudad Real<br><a href="http://es.wikipedia.org/wiki/Parque_nacional_de_las_Tablas_de_Daimiel">Wikipedia</a>'
+    },{
+      idx: 11,
+      pin: 'blue',
+      lat: 28.2713,
+      lng: -16.6436,
+      iw: 'Parque nacional del Teide<br>Islas Canarias<br><a href="http://es.wikipedia.org/wiki/Parque_nacional_del_Teide">Wikipedia</a>'
+    },{
+      idx: 12,
+      pin: 'blue',
+      lat: 29.0111,
+      lng: -13.7333,
+      iw: 'Parque nacional de Timanfaya<br>Islas Canarias<br><a href="http://es.wikipedia.org/wiki/Parque_nacional_de_Timanfaya">Wikipedia</a>'
     }
   ];
 
   GMapz.init('map-container');
-  //console.log(GMapz.pins);
   GMapz.draw(locations);
   GMapz.buttonInit();
 
