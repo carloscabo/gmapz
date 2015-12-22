9972d662f2283dced5afc9e52384ba4090483878
carloscabo <carlos.cabo@the-cocktail.com>
2015-12-22 12:21:05 +0100

    2.05

--- js/ready.js
+++ js/ready.js
@@ -1,17 +1,18 @@
 var
-  map_sample_1,
-  map_sample_2,
-  map_sample_3,
-  map_sample_4,
-  map_sample_5,
-  map_sample_6,
-  map_sample_7,
-  map_sample_8,
-  map_sample_9,
-  autocomplete_1,
-  autocomplete_2,
-  map_sample_10,
-  map_sample_11;
+  map_sample_1A,
+  map_sample_1B,
+  map_sample_1C,
+  map_sample_1D,
+  map_sample_1E,
+  map_sample_2A,
+  map_sample_4A,
+  map_sample_4B,
+  map_sample_4C,
+  map_sample_4D,
+  map_sample_4E,
+  map_sample_6A,
+  map_sample_6B,
+  map_sample_7A;
 
 $(document).ready(function() {
   // La magia aquí
@@ -19,10 +20,10 @@ $(document).ready(function() {
   // Activate console log messages
   GMapz.debug = true;
 
-  // Map sample 1 *************************************************************
+  // Map sample 1A *************************************************************
 
-  map_sample_1 = new GMapz.map(
-    $('#map-sample-1'),
+  map_sample_1A = new GMapz.map(
+    $('#map-sample-1A'),
     { // Google Maps options
       scrollwheel: true, // Default
       scaleControl: true, // Default
@@ -34,10 +35,10 @@ $(document).ready(function() {
     }
   );
 
-  // Map sample 2 *************************************************************
+  // Map sample 1B *************************************************************
 
-  map_sample_2 = new GMapz.map(
-    $('#map-sample-2'),
+  map_sample_1B = new GMapz.map(
+    $('#map-sample-1B'),
     { // Google Maps options
       center: [43.2486, -5.7767],
       zoom: 9,
@@ -45,35 +46,59 @@ $(document).ready(function() {
     }
   );
 
-  // Map sample 3 *************************************************************
+  // Map sample 1C *************************************************************
 
-  var map_sample_3_options = {
+  map_sample_1C = new GMapz.map(
+    $('#map-sample-1C'),
+    {
+      // Empty Google Maps options
+    },
+    // One single marker / location
+    {
+      bolera_de_mieres: {
+        lat: 43.239175,
+        lng: -5.779116,
+        iw: 'Puede que los mejores callos del mundo ;)'
+      }
+    }
+  );
+
+  map_sample_1C.onReady = function(){
+    // this = google.maps instance
+    // this.setSingleMarkerZoom(false); // Disable singleMarkerZoom
+    this.setSingleMarkerZoom(16); // Set custom value
+    this.fitBounds();
+  };
+
+  // Map sample 1E *************************************************************
+
+  var map_sample_1E_options = {
       center: [43.2486, -5.7767]
   };
 
-  map_sample_3 = new GMapz.map(
-    $('#map-sample-3'),
-    map_sample_3_options,
+  map_sample_1E = new GMapz.map(
+    $('#map-sample-1E'),
+    map_sample_1E_options,
     france_cities // gmapz.locations.js
   );
 
-  map_sample_3.onReady = function(){
+  map_sample_1E.onReady = function(){
     // this = google.maps instance
     this.fitBounds();
-  }
+  };
 
-  // Map sample 4 *************************************************************
+  // Map sample 1F *************************************************************
 
-  var map_sample_4_options = {
+  var map_sample_1F_options = {
     // Options here
     mapTypeId: 'ROADMAP', // 'ROADMAP' / 'SATELLITE' / 'HYBRID' / 'TERRAIN'... caps
     center: [48, 2],
     zoom: 5
   };
 
-  map_sample_4 = new GMapz.map(
-    $('#map-sample-4'),
-    map_sample_4_options,
+  map_sample_1F = new GMapz.map(
+    $('#map-sample-1F'),
+    map_sample_1F_options,
     france_cities
   );
 
@@ -87,7 +112,7 @@ $(document).ready(function() {
     // anchorIcon: [-24.0, -48.0] // This propoerty its not supported actually
   }];
 
-  map_sample_4.onReady = function() {
+  map_sample_1F.onReady = function() {
     // Convert markers object, into array
     markers_array = $.map(this.markers, function(val, idx){
       return [val];
@@ -102,65 +127,63 @@ $(document).ready(function() {
     );
   };
 
-  // Map sample 5 *************************************************************
+  // Map sample 2A *************************************************************
 
-  var map_sample_5 = new GMapz.map(
-    $('[data-gmapz="gz-sample-5"]')
+  var map_sample_2A = new GMapz.map(
+    $('[data-gmapz="gz-sample-2A"]')
   );
 
-  map_sample_5.onReady = function() {
+  map_sample_2A.onReady = function() {
     this.addLocations(spain_locs).fitBounds();
   };
 
-  $('#js-add-markers-5').on('click', function(e) {
+  $('#js-add-markers-2A').on('click', function(e) {
     e.preventDefault();
-    map_sample_5.addLocations(morocco);
+    map_sample_2A.addLocations(morocco);
   });
-  $('#js-delete-markers-5').on('click', function(e) {
+  $('#js-delete-markers-2A').on('click', function(e) {
     e.preventDefault();
-    map_sample_5.deleteMarkers(['FEZ','ORAN',12]);
+    map_sample_2A.deleteMarkers(['FEZ','ORAN',12]);
   });
-  $('#js-update-marker-5').on('click', function(e) {
+  $('#js-update-marker-2A').on('click', function(e) {
     e.preventDefault();
-    map_sample_5.addLocations(update);
+    map_sample_2A.addLocations(update);
   });
 
-  // Map sample 6 *************************************************************
+  // Map sample 4A *************************************************************
 
-  var map_sample_6 = new GMapz.map(
-    $('[data-gmapz="gz-sample-6"]')
-  );
+  map_sample_4A = new GMapz.map($('#map-sample-4A'));
 
-  map_sample_6.onReady = function() {
-    this.addLocations(spain_locs).fitBounds();
+  map_sample_4A.onReady = function() {
+    this.addLocations(italy_cities).fitBounds();
   };
 
-  // Map sample 7 *************************************************************
+  autocomplete_4A = new GMapz.autocomplete($('#my-autocomplete-4A'));
 
-  map_sample_7 = new GMapz.map(
-    $('[data-gmapz="gz-sample-7"]')
-  );
-
-  map_sample_7.onReady = function() {
-    this.addLocations(spain_locs).fitBounds();
+  autocomplete_4A.onChange = function () {
+    // this = autocomplete obj
+    var place = this.instance.getPlace();
+    if(typeof place.geometry === 'undefined') {
+      // Place not found
+      alert('Dirección no encontrada');
+      return;
+    }
+    // We show place and closest marker
+    map_sample_4A.geoShowPosition(place);
   };
 
-  map_sample_7.errorAddressNotFound = function(addr) {
-    console.log('Was unable to find: '+addr);
-  };
+  // Map sample 4B *************************************************************
 
-  // Map sample 8 *************************************************************
-
-  map_sample_8 = new GMapz.map(
-    $('#map-sample-8'), {
+  map_sample_4B = new GMapz.map(
+    $('#map-sample-4B'), {
       center: [41.8919, 12.5113],
       zoom: 12
     }
   );
 
-  autocomplete_1 = new GMapz.autocomplete($('#my-autocomplete-1'));
+  autocomplete_4B = new GMapz.autocomplete($('#my-autocomplete-4B'));
 
-  autocomplete_1.onChange = function () {
+  autocomplete_4B.onChange = function () {
     // this = autocomplete
     var
       place = this.instance.getPlace(),
@@ -180,67 +203,88 @@ $(document).ready(function() {
       iw : 'My infowindow 1'
     };
     // Chainning methods
-    map_sample_8.setAllMarkersVisibility(false).addLocations(pos).fitToPlace(place,18).openInfoWindow('autocomplete');
+    map_sample_4B.setAllMarkersVisibility(false).addLocations(pos).fitToPlace(place,18).openInfoWindow('autocomplete');
   };
 
-  // Map sample 9 *************************************************************
+  // Map sample 4C *************************************************************
 
-  map_sample_9 = new GMapz.map($('#map-sample-9'));
+  // Start gmapz with default options
+  var map_sample_4C = new GMapz.map(
+    $('[data-gmapz="gz-sample-4C"]')
+  );
 
-  map_sample_9.onReady = function() {
-    this.addLocations(italy_cities).fitBounds();
+  // onReady add locations and fit bounds
+  map_sample_4C.onReady = function() {
+    this.addLocations(spain_locs).fitBounds();
   };
 
-  autocomplete_2 = new GMapz.autocomplete($('#my-autocomplete-2'));
+  // attachActionButtons is at the end of this file
+  // GMapz.attachActionButtons();
 
-  autocomplete_2.onChange = function () {
-    // this = autocomplete obj
-    var place = this.instance.getPlace();
-    if(typeof place.geometry === 'undefined') {
-      // Place not found
-      alert('Dirección no encontrada');
-      return;
-    }
-    // We show place and closest marker
-    map_sample_9.geoShowPosition(place);
+  // Map sample 4D *************************************************************
+
+  map_sample_4D = new GMapz.map(
+    $('[data-gmapz="gz-sample-4D"]')
+  );
+
+  map_sample_4D.onReady = function() {
+    this.addLocations(spain_locs).fitBounds();
   };
 
-  // Map sample 10 *************************************************************
+  map_sample_4D.errorAddressNotFound = function(addr) {
+    console.log('Was unable to find: '+addr);
+  };
 
-  map_sample_10 = new GMapz.map($('#map-sample-10'));
+  // Map sample 5A, responsive *************************************************************
 
-  map_sample_10.onReady = function() {
+  map_sample_5A = new GMapz.map($('#map-sample-5A'));
+
+  map_sample_5A.onReady = function() {
     // Enables responsive control
     this.addScrollControl();
     // Load Italy cities
     this.addLocations(italy_cities).fitBounds();
     // Responsive events
     MQBE.on('enter', 'mobile', function() {
-      map_sample_10.lockScroll();
+      map_sample_5A.lockScroll();
     }).on('leave', 'mobile', function() {
-      map_sample_10.resumeScroll();
+      map_sample_5A.resumeScroll();
     });
   };
 
-  map_sample_10.onDraw = function() {
+  map_sample_5A.onDraw = function() {
     console.log(this.convertLatLngToPixels(
       new google.maps.LatLng(41.890, 12.500)
     ));
   };
 
-  $(document).on('click touchstart', '#js-btn-scroll-lock', function(e) {
+  $(document).on('click touchstart', '#js-btn-scroll-lock-5A', function(e) {
     e.preventDefault();
     $('body').addClass('force-show-scroll-control');
-    map_sample_10.lockScroll();
+    map_sample_5A.lockScroll();
   });
 
-  $(document).on('click touchstart', '#js-btn-scroll-resume', function(e) {
+  $(document).on('click touchstart', '#js-btn-scroll-resume-5A', function(e) {
     e.preventDefault();
     $('body').removeClass('force-show-scroll-control');
-    map_sample_10.resumeScroll();
+    map_sample_5A.resumeScroll();
   });
 
-  // Map sample 11 *************************************************************
+  // Map sample 6A *************************************************************
+
+  map_sample_6A = new GMapz.map(
+    $('#map-sample-6A'), {
+      center: [36.08, -6.82],
+      zoom: 5,
+      pixelOffset: [158, 100] // Offset of infowindow
+    }
+  );
+
+  map_sample_6A.onReady = function() {
+    this.addLocations(spain_locs).fitBounds();
+  };
+
+  // Map sample 6B *************************************************************
 
   GMapz.onGoogleMapsReady = function () {
     // Enable infobox library
@@ -248,9 +292,9 @@ $(document).ready(function() {
   };
 
   // Start map with default options
-  map_sample_11 = new GMapz.map($('#map-sample-11'));
+  map_sample_6B = new GMapz.map($('#map-sample-6B'));
 
-  map_sample_11.onReady = function() {
+  map_sample_6B.onReady = function() {
 
     // Define custom infobox style
     var
@@ -282,15 +326,48 @@ $(document).ready(function() {
     this.addLocations(italy_cities).fitBounds();
   };
 
-  map_sample_11.onDraw = function() {
+  map_sample_6B.onDraw = function() {
     // Open Rome marker / infobox
     this.markers['roma'].click();
   };
 
-  // Attachear botones ********************************************************
+  // Map sample 7A *************************************************************
 
-  // Attach button with data-gmapz attribute
+  $('.js-open-lightbox-7A.fancybox').on('click', function(e) {
+    e.preventDefault();
+    $.fancybox.open('<div id="fancy-map" style="width:100%;height:100%;"></div>', {
+      autoSize: false,
+      width: 800,
+      height: 600,
+      afterShow: function() {
+        // WARNING!
+        // For some reason the afterShow event of fancybox seems
+        // to be executed BEFORE the container is drawn.
+        // This causes issues with Google Maps, to be sure that the
+        // fancybox is drawn we call GMpaz inside a setTimeout, 0ms
+        // http://stackoverflow.com/a/779785/748321
+        setTimeout(function(){
+          map_sample_7A = new GMapz.map(
+            $('#fancy-map'),
+            {
+              center: [48.860, 2.340],
+              zoom: 12
+            },
+            {
+              map_point: {
+                lat: 48.860,
+                lng: 2.340
+              }
+            }
+          );
+        } ,0);
+      }
+    });
+  });
+
+
+  // Attach button with data-gmapz attribute ********************************************************
+
   GMapz.attachActionButtons();
 
-});
-
+}); // Domready
